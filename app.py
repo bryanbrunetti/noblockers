4123de8ff49ce286403499e73c91fb3b13c00db5
Bryan Brunetti <bryan.brunetti@gmail.com>
2022-06-30 23:36:32 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,51 @@
+import os
+import redis as r
+import random
+import datetime
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+redis = r.StrictRedis(host=os.environ.get("REDIS_URL", "localhost"), port=6379, db=0, charset="utf-8")
+
+
+def get_participants():
+    participants = redis.smembers("participants")
+    participants = participants if participants else []
+    return list(participants)
+
+
+@app.route('/')
+def good_morning():
+    participants = get_participants()
+    # TODO: Allow the date to be passed in as an argument to allow seeing what future ordering will look like
+    now = datetime.datetime.now()
+    dt = now.strftime('%-j')
+
+    random.seed(int(dt) + 1)
+    random.shuffle(participants)
+
+    return render_template("standup.html", participants=participants, today=now.strftime("%A %B %-d %Y"))
+
+
+@app.route("/update", methods=['GET'])
+def get_update():
+    participants = get_participants()
+    return render_template("update.html", participants=participants)
+
+
+@app.route("/add", methods=["POST"])
+def add_participant():
+    redis.sadd("participants", request.form['participant'])
+
+    return redirect("/update")
+
+
+@app.route("/remove")
+def remove_participant():
+    redis.srem("participants", request.args['name'])
+
+    return redirect("/update")
+
+
+if __name__ == '__main__':
+    app.run()
